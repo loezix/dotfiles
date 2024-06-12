d526d7e3df2e6949a8703c21d866cf433bd5d99d
loezix <loezix@lzx.studio>
2024-06-12 16:26:48 -0300

    kitty+tmux

--- dot_config/zsh/tmux.zsh
+++ dot_config/zsh/tmux.zsh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
-session_name="0"
+if [ "${TERM}" = "xterm-kitty" ]; then
 
-tmux has-session -t=$session_name 2> /dev/null
+  session_name="0"
 
-if [[ $? -ne 0 ]]; then
-  TMUX='' tmux new-session -d -s "$session_name"
-fi
+  tmux has-session -t=$session_name 2> /dev/null
 
-if [[ -z "$TMUX" ]]; then
-  tmux attach -t "$session_name"
-else
-  tmux switch-client -t "$session_name"
+  if [[ $? -ne 0 ]]; then
+    TMUX='' tmux new-session -d -s "$session_name"
+  fi
+
+  if [ -z "$TMUX" ]; then
+    tmux attach -t "$session_name"
+  else
+    tmux switch-client -t "$session_name"
+  fi
 fi
