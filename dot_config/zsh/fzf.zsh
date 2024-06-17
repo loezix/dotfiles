a2442fdac87d25763622df8b534a71e42a7ebe73
loezix <loezix@lzx.studio>
2024-06-17 15:27:19 -0300

    .chezmoiignore

--- dot_config/zsh/fzf.zsh
+++ dot_config/zsh/fzf.zsh
@@ -5,11 +5,15 @@ source "${HOME}/.local/lib/fzf-git.sh"
 
 
 DRACULA_THEME="--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4"
+CATPPUCCIN_MOCHA=" \
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 
-FZF_THEME=$DRACULA_THEME
+FZF_THEME=$CATPPUCCIN_MOCHA
 
-FZF_DEFAULT_OPTS="${FZF_THEME}"
-#FZF_DEFAULT_COMMAND="fd --hidden --exclude .git"
+FZF_DEFAULT_OPTS="${FZF_THEME} --ansi"
+FZF_DEFAULT_COMMAND="fd --hidden --exclude .git"
 FZF_CTRL_T_COMMAND="${FZF_DEFAULT_COMMAND} --type=f"
 FZF_ALT_C_COMMAND="${FZF_DEFAULT_COMMAND} --type=d"
 
--- dot_config/zsh/zsh.zsh
+++ dot_config/zsh/zsh.zsh
@@ -1,7 +1,48 @@
-# ZSH SETTINGS
+# zsh settings
 
-ZSH_HIGHLIGHT_STYLES[path]=none
-ZSH_HIGHLIGHT_STYLES[path_prefix]=none
-ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#aaaaaa"
+## env
+HISTSIZE=5000
+SAVEHIST=$HISTSIZE
+HISTDUP=erase
 
+## options
+setopt appendhistory
+setopt sharehistory
+setopt hist_ignore_space
+setopt hist_ignore_all_dups
+setopt hist_save_no_dups
+setopt hist_ignore_dups
+setopt hist_find_no_dups
+
+## style
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
+
+## bindings
 bindkey -e
+bindkey '^[[A' history-search-backward
+bindkey '^[[B' history-search-forward
+bindkey '^[[H' beginning-of-line
+bindkey '^[[F' end-of-line
+
+## aliases
+alias   vi="nvim"
+alias   ls="eza --icons --group-directories-first"
+alias    l="ls -l --git"
+alias   la="ls -a"
+alias  lla="ls -la"
+alias   lt="ls -T -L3 --icons"
+alias  cat="bat --number --color=always"
+alias grep="rg"
+
+## plugin settings
+
+### zsh-syntax-highlight
+if [[ ! -z $ZSH_HIGHLIGHT_STYLES ]]; then
+  ZSH_HIGHLIGHT_STYLES[path]=none
+  ZSH_HIGHLIGHT_STYLES[path_prefix]=none
+  ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#aaaaaa"
+fi
+
+## services
+autoload compinit && compinit
